dfbce98ff7eadc75ee5ca968c605444f4a6a2e0a
G186562021 <218656@st.takushoku-u.ac.jp>
2023-07-06 16:46:26 +0900

    あ

--- kazuate.js
+++ kazuate.js
@@ -6,33 +6,41 @@ console.log('答え（デバッグ用）: ' + kotae);
 // 入力回数（予想回数）
 let kaisu = 0;
 
-// 予想を4回実行する
-// 将来以下の hantei(); の4回の呼び出しを全て削除する
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector('#print');
+b.addEventListener('click', nyuuryoku);
 
-// ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() 
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+
+function nyuuryoku() {
+	let i = document.querySelector('input[name="kaitou"]');
+	let answer = i.value;
+  let p2 = document.querySelector('span#answer');	
+  p2.textContent = answer;
+
+  kaisu=kaisu+1;
+  let p3 = document.querySelector('span#kaisu');	
+  p3.textContent = kaisu;
 
 let x=document.querySelector('span#kaisu');
-let y=document.querySelector('kotae');
+let y=document.querySelector('span#answer');
 let z=document.querySelector('p#result');
-if(x<4){
-    if(yoso===kotae){
+if(kaisu<4){
+    if(answer==kotae){
     z.textContent='正解です。';
-  } else if(yoso>kotae){
-    z.textContent='違います。もう少し大きいです。';
+  } else if(answer>kotae){
+    z.textContent='違います。もう少し小さいです。';
     } else {
-      z.textContent='違います。もう少し小さいです。';
+      z.textContent='違います。もう少し大きいです。';
     } 
 }else{
-  z.textContent='答えは'+y+'でした。すでにゲームは終わっています。';
+  z.textContent='答えは'+answer+'でした。すでにゲームは終わっています。';
 
 }
+
+}
+
+// ボタンを押した後の処理をする関数 hantei() の定義
+
+  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+
 
 
